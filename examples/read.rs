2683982992619f492d377aac36e6da479c23a702
Tim Crawford <tcrawford@system76.com>
2021-03-08 14:02:38 -0700

    Add example to read and print extracted SMMSTORE
    Add the standalone smmstore app [1] as a example in this project.
    
    [1]: https://github.com/system76/smmstore

---
+++ examples/read.rs
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+extern crate system76_firmware_smmstore as smmstore;
+
+use std::{char, env, fs, mem, process};
+use uefi::guid::Guid;
+
+fn main() {
+    let path = match env::args().nth(1) {
+        Some(some) => some,
+        None => {
+            eprintln!("read [file]");
+            process::exit(1);
+        }
+    };
+
+    let data = fs::read(path)
+        .expect("failed to read file");
+
+    let compact = smmstore::deserialize(&data);
+
+    for (key, value) in compact.iter() {
+        if key.len() > mem::size_of::<Guid>() && !value.is_empty() {
+            let (_guid, _varname) = unsafe {
+                let ptr = key.as_ptr();
+                (
+                    *(ptr as *const Guid),
+                    ptr.add(mem::size_of::<Guid>()) as *const u16
+                )
+            };
+
+            print!("\x1B[1m");
+            let mut j = mem::size_of::<Guid>();
+            while j + 1 < key.len() {
+                let w =
+                    (key[j] as u16) |
+                    (key[j + 1] as u16) << 8;
+                if w == 0 {
+                    break;
+                }
+                if let Some(c) = char::from_u32(w as u32) {
+                    print!("{}", c);
+                }
+                j += 2;
+            }
+            println!(": {}\x1B[0m", value.len());
+
+            for row in 0..(value.len() + 15)/16 {
+                print!("{:04X}:", row * 16);
+                for col in 0..16 {
+                    let j = row * 16 + col;
+                    if j < value.len() {
+                        print!(" {:02X}", value[j]);
+                    }
+                }
+                println!();
+            }
+        }
+    }
+
+    if smmstore::is_corrupted(&data) {
+        println!("\x1B[1m\x1B[91mSMMSTORE region is corrupted\x1B[39m\x1B[0m");
+    }
+}
